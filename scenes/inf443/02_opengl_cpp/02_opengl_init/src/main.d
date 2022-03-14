526243f097124c9919c080fc777d0e374bb95306
Tianyang <1538109712@qq.com>
2022-03-14 13:14:25 +0100

    cloned from remote, compiled with "make" in local env

---
+++ scenes/inf443/02_opengl_cpp/02_opengl_init/src/main.d
@@ -0,0 +1,7 @@
+src/main.o: src/main.cpp \
+  src/../third_party/src/glad/include/glad/glad.hpp \
+  /opt/homebrew/include/GLFW/glfw3.h
+
+src/../third_party/src/glad/include/glad/glad.hpp:
+
+/opt/homebrew/include/GLFW/glfw3.h:
