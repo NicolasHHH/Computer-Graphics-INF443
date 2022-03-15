067a7ea1001248267b4e8c2313b90ecd67dc4eb8
Tianyang <1538109712@qq.com>
2022-03-15 10:33:33 +0100

    TD2 1st part

---
+++ scenes/inf443/02_opengl_cpp/03_triangle/src/main.d
@@ -0,0 +1,9 @@
+src/main.o: src/main.cpp \
+  src/../third_party/src/glad/include/glad/glad.hpp \
+  /opt/homebrew/include/GLFW/glfw3.h src/helper_opengl.hpp
+
+src/../third_party/src/glad/include/glad/glad.hpp:
+
+/opt/homebrew/include/GLFW/glfw3.h:
+
+src/helper_opengl.hpp:
