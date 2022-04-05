200032dfc1cb7cb3c6e23544f47b74c65d54c499
Tianyang <1538109712@qq.com>
2022-04-05 10:45:17 +0200

    add two green rotating cylinder at the end

--- scenes/inf443/05_animation/b_hierarchy/src/scene.cpp
+++ scenes/inf443/05_animation/b_hierarchy/src/scene.cpp
@@ -35,6 +35,9 @@ void scene_structure::initialize()
 	mesh_drawable cube1;
 	mesh_drawable cylinder1_son;
 
+    mesh_drawable cylinder2;
+    mesh_drawable cylinder3;
+
 	// Create the geometry of the meshes
 	// Note: this geometry must be set in their local coordinates with respect to their position in the hierarchy (and with respect to their animation)
 	cube_base.initialize(mesh_primitive_cube(), "Cube base"); cube_base.transform.scaling = 0.15f;
@@ -44,12 +47,19 @@ void scene_structure::initialize()
 	cube1.initialize(mesh_primitive_cube(), "Cube1"); cube1.transform.scaling = 0.15f;
 	cylinder1_son.initialize(mesh_primitive_cylinder(0.03f, { 0,0,-0.25f }, { 0.0f,0,0.25f }), "Cylinder1 son");
 
+    cylinder2.initialize(mesh_primitive_cylinder(0.03f, { -0.2f,0,-0.25f }, { 0.2f,0,-0.25f }),"Cylinder2");
+    cylinder3.initialize(mesh_primitive_cylinder(0.03f, { -0.2f,0,+0.25f }, { 0.2f,0,+0.25f }),"Cylinder3");
+
 	// Set the color of some elements
 	vec3 color1 = { 0.8f, 0.5f, 0.7f };
 	cylinder1.shading.color = color1;
 	cube1.shading.color = color1;
 	cylinder1.shading.color = color1;
 	cylinder1_son.shading.color = color1;
+
+    vec3 color2 = {0.5f,0.9f,0.3f};
+    cylinder2.shading.color = color2;
+    cylinder3.shading.color = color2;
 
 	// Add the elements in the hierarchy
 	//   The syntax is hierarchy.add(mesh_drawable, "name of the parent element", [optional: local translation in the hierarchy])
@@ -62,7 +72,8 @@ void scene_structure::initialize()
 	hierarchy.add(cylinder1, "Sphere junction");
 	hierarchy.add(cube1, "Cylinder1", { 1.0f,0,0 }); // the translation is used to place the cube at the extremity of the cylinder
 	hierarchy.add(cylinder1_son, "Cube1");
-
+    hierarchy.add(cylinder2,"Cylinder1 son",{0,0,0});
+    hierarchy.add(cylinder3,"Cylinder1 son",{0,0,0});
 }
 
 
@@ -83,6 +94,8 @@ void scene_structure::display()
 	hierarchy["Cylinder1"].transform.rotation = rotation_transform::from_axis_angle({ 0,0,1 }, timer.t);
 	hierarchy["Cube1"].transform.rotation = rotation_transform::from_axis_angle({ 1,0,0 }, -3 * timer.t);
 
+    hierarchy["Cylinder2"].transform.rotation = rotation_transform::from_axis_angle({ 0,0,1 }, -3 * timer.t);
+    hierarchy["Cylinder3"].transform.rotation = rotation_transform::from_axis_angle({ 0,0,1 }, -3 * timer.t);
 
 	// This function must be called before the drawing in order to propagate the deformations through the hierarchy
 	hierarchy.update_local_to_global_coordinates();
