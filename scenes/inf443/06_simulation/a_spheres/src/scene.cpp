5e574f942baa86eb24537b4315d8595d59b3f73e
Tianyang <1538109712@qq.com>
2022-04-12 10:58:24 +0200

    bounding particles

--- scenes/inf443/06_simulation/a_spheres/src/scene.cpp
+++ scenes/inf443/06_simulation/a_spheres/src/scene.cpp
@@ -11,8 +11,6 @@ void scene_structure::initialize()
 	environment.camera.axis = camera_spherical_coordinates_axis::z;
 	environment.camera.look_at({ 2.0f,-4.0f,2.0f }, { 0,0,0 });
 
-
-
 	// Particles are displayed as spheres
 	float const sphere_radius = 0.05f;
 	sphere.initialize(mesh_primitive_sphere(sphere_radius), "sphere");
@@ -26,10 +24,6 @@ void scene_structure::initialize()
 	// Initial timing between the creation of two particles.
 	timer.event_period = 0.6f;
 }
-
-
-
-
 
 void scene_structure::display()
 {
@@ -45,38 +39,51 @@ void scene_structure::display()
 
 	bool const new_particle = timer.event;
 	if (new_particle == true) {
-		vec3 const p0 = { 0,0,0 };
+		vec3 const p0 = { 0,0,0 }; // Initial pose of the particle
 
 		// Initial random velocity (x,y) components are uniformly distributed along a circle.
 		const float theta = rand_interval(0, 2 * Pi);
+        const float height = rand_interval(3.0f, 7.0f);
 		const vec3 v0 = vec3(std::cos(theta), std::sin(theta), 5.0f);
 
 		particles.push_back({ p0,v0 });
 	}
 
-	// Evolve position of particles
+	// Evolve position of particles with the potential field
 	const vec3 g = { 0.0f,0.0f,-9.81f };
 	for (particle_structure& particle : particles)
 	{
 		const float m = 0.01f; // particle mass
 
-		vec3& p = particle.p;
-		vec3& v = particle.v;
+		vec3& p = particle.p; // position
+		vec3& v = particle.v; // velocity
 
-		const vec3 F = m * g;
+		const vec3 F = m * g; // gravity
 
 		// Numerical integration
-		v = v + dt * F / m;
+		v = v + dt * F / m;  // a = F/m
 		p = p + dt * v;
 	}
 
 	// To do: 
 	//  Add the collision handling/bouncing to the particles here
-	//  ...
+    for (particle_structure& particle : particles)
+    {
+        vec3& p = particle.p;
+        if (p.x*p.x + p.y*p.y < 4.0f && p.x*p.x + p.y*p.y >0.01*0.01){
+            vec3& v = particle.v;
+            if (p.z < -0.01) {
+                p.z = 0;
+                v.z = -v.z * 0.6 ;
+                v.y *= 0.9 ;
+                v.x *= 0.9;
+            }
+        }
+    }
 
 	// Remove particles that are too low
 	for (auto it = particles.begin(); it != particles.end(); ) {
-		if (it->p.z < -3)
+		if (it->p.z < -1)
 			it = particles.erase(it);
 		if (it != particles.end())
 			++it;
